5b0fb1b76a31d23ab9a9847bcde4d29d03859cc5
ldmatos <ldmatos8@gmail.com>
2015-03-18 21:09:28 -0300

    mandando o usuario para index

--- Projeto/app/controllers/Application.java
+++ Projeto/app/controllers/Application.java
@@ -1,17 +1,30 @@
 package controllers;
 
+import java.util.*;
+import models.Usuario;
+import models.dao.GenericDAO;
+import models.dao.GenericDAOImpl;
 import play.api.mvc.Session;
+import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
 public class Application extends Controller {
 
+	private static GenericDAO dao = new GenericDAOImpl();
+	
+	@Transactional
     public static Result index() {
+		//session().clear();
         if (session().get("email") == null) {
         	return redirect(routes.Login.show());
-        } 
-       return ok(index.render("Your new application is ready."));
+        }
+        Usuario usuario = (Usuario) dao.findByAttributeName("Usuario",
+				"email", session().get("email")).get(0);
+       return ok(index.render(usuario));
        // return redirect(routes.Login.show());
     }
+    
+    
 }
