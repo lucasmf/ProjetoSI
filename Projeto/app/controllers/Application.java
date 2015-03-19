87eca72c8509eb47497db0f5bd28d924258872c2
ldmatos <ldmatos8@gmail.com>
2015-03-18 21:56:18 -0300

    fazendo login funcionar

--- Projeto/app/controllers/Application.java
+++ Projeto/app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.util.*;
 import models.Usuario;
+import models.Tema;
 import models.dao.GenericDAO;
 import models.dao.GenericDAOImpl;
 import play.api.mvc.Session;
@@ -17,13 +18,19 @@ public class Application extends Controller {
 	@Transactional
     public static Result index() {
 		//session().clear();
-        if (session().get("email") == null) {
+		if (session().get("email") == null) {
         	return redirect(routes.Login.show());
         }
+		if(dao.findByAttributeName("Usuario",
+				"email", session().get("email")).size() == 0) {
+					session().clear();
+					return redirect(routes.Login.show());
+		}
         Usuario usuario = (Usuario) dao.findByAttributeName("Usuario",
 				"email", session().get("email")).get(0);
-       return ok(index.render(usuario));
-       // return redirect(routes.Login.show());
+        List<Tema> temas = dao.findAllByClassName("Tema");
+        return ok(index.render(usuario, temas));
+        // return redirect(routes.Login.show());
     }
     
     
