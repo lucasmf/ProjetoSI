52d3f15c092e72707452d21b7692466d8ea82c62
ldmatos <ldmatos8@gmail.com>
2015-03-20 20:02:22 -0300

    adicionando voto em dica

--- Projeto/app/models/Dica.java
+++ Projeto/app/models/Dica.java
@@ -14,21 +14,49 @@ public abstract class Dica {
 	@GeneratedValue
 	private Long id;
 
+	private int votos[];
+	
 	@ManyToOne(fetch=FetchType.LAZY, cascade= CascadeType.ALL)
 	private Disciplina disciplina;
 	
 	//private HashMap<Long, Avaliacao> avaliacoes;
 	
 	public Dica() {
-		
+		this.votos = new int[2];
+		votos[0] = 0;
+		votos[1] = 0;
+	}
+	
+	public double getAprovacao(){
+		return (double)votos[1]/(votos[0]+votos[1]);
+	}
+	
+	public int getVotosNegavitos() {
+		return votos[0];
+	}
+	
+	public int getVotosPositivos() {
+		return votos[1];
 	}
 	
 	public Disciplina getDisciplina() {
 		return disciplina;
 	}
+	
+	public void votar(int v) {
+		votos[v]++;
+	}
 
 	public void setDisciplina(Disciplina disciplina) {
 		this.disciplina = disciplina;
+	}
+
+	public int[] getVotos() {
+		return votos;
+	}
+
+	public void setVotos(int votos[]) {
+		this.votos = votos;
 	}
 	
 /*	public void addAvaliacao(Usuario usuario, Avaliacao avaliacao) {
