2ba49c20ea75097ea6d2bcaf660c41cb9782e4a1
leticia <leticiamaiat@gmail.com>
2015-03-22 19:29:14 -0300

    Ajeitando models de Metadica

--- Projeto/app/models/Metadica.java
+++ Projeto/app/models/Metadica.java
@@ -1,10 +1,40 @@
 package models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 @Entity(name="Metadica")
 public class Metadica extends Dica {
+	
+	@OneToMany
+	private List<Dica> dicasAgregadas;
+	private String dica;
+	
 	public Metadica() {
 		
+	}
+	
+	public Metadica(String dica, List<Dica> dicasAgregadas) {
+		this.setDica(dica);
+		this.setDicasAgregadas(dicasAgregadas);
+		
+	}
+
+	public List<Dica> getDicasAgregadas() {
+		return dicasAgregadas;
+	}
+
+	public void setDicasAgregadas(List<Dica> dicasAgregadas) {
+		this.dicasAgregadas = dicasAgregadas;
+	}
+
+	public String getDica() {
+		return dica;
+	}
+
+	public void setDica(String dica) {
+		this.dica = dica;
 	}
 }
